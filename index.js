64056fd1d89e63942b8ce671baf7759be814eaae
Dolon Roy <dolonr718@gmail.com>
2023-11-05 11:22:26 +0600

    project setup and start commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Welcome to assignment 11 backend😬!!");
+})
+
+app.listen(port, () => {
+    console.log("listening on port " + port);
+});
